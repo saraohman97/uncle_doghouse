778da7632ce46549ac82f2965107f2fcf17b1a44
Sara Öhman <saraohman97@gmail.com>
2024-03-09 20:21:16 +0100

    *

--- app/(routes)/album/page.tsx
+++ app/(routes)/album/page.tsx
@@ -1,41 +1,41 @@
 import Image from "next/image";
 
-const images = [
-  {
-    id: 1,
-    src: "/bg.svg",
-  },
-  {
-    id: 2,
-    src: "/logo.jpg",
-  },
-  {
-    id: 3,
-    src: "/bg.svg",
-  },
-  {
-    id: 4,
-    src: "/logo.jpg",
-  },
-  {
-    id: 5,
-    src: "/logo.jpg",
-  },
-  {
-    id: 6,
-    src: "/bg.svg",
-  },
-  {
-    id: 7,
-    src: "/logo.jpg",
-  },
-  {
-    id: 8,
-    src: "/bg.svg",
-  }
-];
-
 const AlbumPage = () => {
+  const images = [
+    {
+      id: 1,
+      src: "/bg.svg",
+    },
+    {
+      id: 2,
+      src: "/logo.jpg",
+    },
+    {
+      id: 3,
+      src: "/bg.svg",
+    },
+    {
+      id: 4,
+      src: "/logo.jpg",
+    },
+    {
+      id: 5,
+      src: "/logo.jpg",
+    },
+    {
+      id: 6,
+      src: "/bg.svg",
+    },
+    {
+      id: 7,
+      src: "/logo.jpg",
+    },
+    {
+      id: 8,
+      src: "/bg.svg",
+    }
+  ];
+  
   return (
     <div className="md:columns-3 gap-4 max-w-screen-lg mx-auto my-10 md:my-24">
       {images.map((item) => (
